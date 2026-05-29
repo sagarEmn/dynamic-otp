cda45a6dcd7ea65f6e8b7eebdff295c59ef33e7f
Sagar <100994834+sagarEmn@users.noreply.github.com>
2026-05-29 22:41:49 +0545

    feat: add constants for risk in a separate file for lint pass

---
+++ src/context/riskConstants.js
@@ -0,0 +1,10 @@
+export const EMPTY_TRANSACTION = {
+  payeeId: "",
+  payeeName: "",
+  amount: 0,
+  purpose: "",
+  remarks: "",
+  activeCall: false,
+  newDevice: false,
+  unusualLocation: false,
+};
